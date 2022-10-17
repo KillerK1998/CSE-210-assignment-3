785613a3555077162c2cc7ec0b97cc6c4f5ad325
KillerK1998 <kaler.sappington@gmail.com>
2022-10-17 16:27:52 -0600

    changed words.py

--- words.py
+++ words.py
@@ -5,7 +5,7 @@ import random
 # Open the file in read mode
 with open("words.txt", "r") as file:
     allText = file.read()
-    words = list(map(str, allText.split()))
+    words = allText.split()
   
     # print random string
     print(random.choice(words))
