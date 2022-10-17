f07146ec5022b4eba5a258074715ee700c72cc37
emoryhubbardbyui <hub21003@byui.edu>
2022-10-17 18:32:36 -0400

    added functions to jumper

--- jumper.py
+++ jumper.py
@@ -1,2 +1,9 @@
 #Responsible for storing the state of the Jumper (how many lines have been removed
 # from parachute), and returning a string that shows how he is currently displayed
+
+def __init__(self):
+    pass
+def cutLine(self):
+    pass
+def draw(self):
+    pass
