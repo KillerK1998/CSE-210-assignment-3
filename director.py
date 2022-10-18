eb3e5c12911c9d0f0f059c918e904de1ebc916d3
Ejpulver <pul15004@byui.edu>
2022-10-18 11:32:08 -0600

    wrote some things in director

---
+++ director.py
@@ -0,0 +1,12 @@
+from director import director
+from jumper import jumper
+from words import words
+
+class Director:
+
+    def __init__(self):
+
+        self._player_guess = ""
+        self._is_playing = True
+        
+
