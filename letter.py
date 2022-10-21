f0d9d7f7274abc28bd26777973fa7663278ab384
Ejpulver <pul15004@byui.edu>
2022-10-20 21:59:07 -0600

    moved letter functions to letter class

--- letter.py
+++ letter.py
@@ -7,5 +7,19 @@ class Letter:
     def getUserGuess():
         user_guess = input("Guess a letter [a-z]: ")
         return user_guess
+    
+    def draw_letters(letters):
+        letter_line = Director.make_letter_line(letters)
+        print(letter_line)
+
+    def make_letter_line(letters):
+        letter_line = ""
+        for letter in letters:
+            if letter.guessed == True:
+                letter_line += letter.char
+            else:
+                letter_line += "_"
+            letter_line += " "
+        return letter_line
 
 
